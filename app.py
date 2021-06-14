972c783259890b0d3f2591c48c49f55ab8ce2ad7
Navit Choudhary <navitchoudhary22@gmail.com>
2021-06-14 15:49:42 +1000

    added are you okay api

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from kafka import KafkaProducer, KafkaConsumer
 from threading import Event
 import signal
 from flask_kafka import FlaskKafka
+import datetime
+from json import JSONEncoder
 app = Flask(__name__)
 
 app.config["MONGO_URI"] = "mongodb://localhost:27017/digital_twin_dev"
@@ -40,6 +42,19 @@ def listen_kill_server():
     signal.signal(signal.SIGHUP, bus.interrupted_process)
 
 
+class DateTimeEncoder(JSONEncoder):
+        #Override the default method
+        def default(self, obj):
+            if isinstance(obj, (datetime.date, datetime.datetime)):
+                return obj.isoformat()
+
+def on_send_success(record_metadata):
+    print(record_metadata.topic)
+
+
+def on_send_error(excp):
+    print('I am an errback: ' + excp)
+
 @bus.handle('ARE_YOU_OK')
 def topic_ARE_YOU_OK(msg):
     print("consumed {} from ARE_YOU_OK".format(msg))
@@ -52,6 +67,18 @@ def topic_I_AM_LIVE(msg):
 @app.route('/', methods=['GET'])
 def default():
     return "Welcome to Digital Twin Tempelate"
+
+@app.route('/areYouOkay', methods=['GET'])
+def areYouOkay():
+    demoData = {
+      "topic": "ARE_YOU_OK",
+      "messages": "",
+      "attributes": 1,
+      "timestamp": datetime.datetime.now()
+    }
+    demoData = DateTimeEncoder().encode(demoData)
+    producer.send("ARE_YOU_OK",demoData).add_callback(on_send_success).add_errback(on_send_error)
+    return "Sent data succesfully"
 
 if __name__ == '__main__':
     bus.run()
